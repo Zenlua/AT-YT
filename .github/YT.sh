3611974224e8921aa3b8295d6c3b62f9789d3d8e
Zelooooo <143571950+Zelooooo@users.noreply.github.com>
2023-09-07 11:58:18 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -142,6 +142,7 @@ fi
 if [ "$TYPE" == 'true' ];then
 lib='lib/*/*'
 if [ -e apk/YouTube.apks ];then
+echo "- Giải nén base.apk"
 unzip -qo apk/YouTube.apks 'base.apk' -d Tav
 unzip -qo apk/YouTube.apk lib/$DEVICE/* -d Tav
 mv -f Tav/lib/$DEVICE Tav/lib/$ach
