dbe20c456b1f6e4046e40c6ab936e6ad59c20582
Zelooooo <143571950+Zelooooo@users.noreply.github.com>
2023-10-05 09:58:57 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -198,7 +198,7 @@ fi
 
 echo "▼ Bắt đầu quá trình xây dựng..."
 eval "java -Djava.io.tmpdir=$HOME -jar $lib1 patch -b $lib2 -m $lib3 apk/YouTube.apk -o YT.apk \
-"$Tof $Ton $Mro $theme $feature" > Log.txt 2>> Log.txt"
+$Tof $Ton $Mro $theme $feature > Log.txt 2>> Log.txt"
 sed '/WARNING: warn: removing resource/d' Log.txt
 echo > 2.txt
 
