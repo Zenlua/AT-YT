bc25e6d5a8b6be65b77168c87abb1a08599959e3
Zelooooo <143571950+Zelooooo@users.noreply.github.com>
2023-10-21 02:04:09 +0700

    Update XYT.sh

--- .github/XYT.sh
+++ .github/XYT.sh
@@ -217,7 +217,7 @@ echo '{
 "changelog": "https://github.com/'$GITHUB_REPOSITORY'/releases/download/Up/Up-X'$V'notes.json"
 }' > "Up-X$V$ach$amoled2.json"
 
-echo 'Update '$(date)'\nYouTube: '$VER'\nVersion: '${VER//./}'' > Up-X${V}notes.json
+echo 'Update '$(date)' YouTube: '$VER' Version: '${VER//./}'' > Up-X${V}notes.json
 
 # Tạo module magisk
 cd $HOME/.github/Modun
