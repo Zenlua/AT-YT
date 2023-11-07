1f1ee54e4a47c61e9eb281e42bef633b0aa2d2e1
Kakathic <kakathic@gmail.com>
2023-11-08 01:43:02 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -199,7 +199,7 @@ fi
 echo "▼ Bắt đầu quá trình xây dựng..."
 eval "java -Djava.io.tmpdir=$HOME -jar $lib1 patch -b $lib2 -m $lib3 apk/YouTube.apk -o YT.apk "$Tof $Ton $Mro $theme $feature" > Log.txt 2>> Log.txt"
 sed '/WARNING: warn: removing resource/d' Log.txt
-echo > 2.txt
+echo '- Quá trình xây dựng apk xong.' | tee 2.txt
 
 ) & (
 
