8dc5acff90ff6e9650bb029248a39b9113f53ae9
Kakathic <kakathic@gmail.com>
2023-11-07 23:31:22 +0700

    Update XYT.sh

--- .github/XYT.sh
+++ .github/XYT.sh
@@ -186,9 +186,8 @@ echo '- Quá trình xây dựng apk xong.' | tee 2.txt
 
 sleep 5
 Loading "$(echo */res/values)" "$(echo */res/values)" >/dev/null
-rm -fr apk/YouTube.apk
-zip -qr $(echo */YouTube.apk) -d res/*
-echo '- Quá trình ghép string xong' | tee 1.txt
+zip -qr apk/YouTube.apk -d res/*
+echo '- Quá trình xoá rác xong' | tee 1.txt
 
 )
 
