1c2289072310d6591ee7c61293db79ee03064090
Kẹo Kéo <kakathic@gmail.com>
2024-03-31 15:32:21 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -93,10 +93,20 @@ Kad=Edit$Vop
 V=N$Vop2
 fi
 
+echo $VERSION
+
+
+
 if [[ "$VERSION" == 'Autu' ]] && [[ "$(Xem https://github.com/$GITHUB_REPOSITORY/releases/download/Up/Up-K${V}notes.json | grep -cm1 "${VER//./}")" == 1 ]];then
 echo "! Là phiên bản mới nhất."
 exit 0
 fi
+
+if [ 1 == 1 ] && [ 2 == 2 ];then
+echo 123
+fi
+
+exit 0
 
 Upenv V "$V"
 Upenv Kad "$Kad"
