ea281eef216e17b4df373252ac4b281735fb8919
Kẹo Kéo <kakathic@gmail.com>
2024-03-31 22:56:15 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -78,7 +78,7 @@ lib="lib/arm64-v8a/* lib/x86/* lib/x86_64/*"
 ach="arm"
 fi
 
-echo -e "#### Changelog $Vidon YouTube\n```" > Log.txt
+echo -e "#### Changelog $Vidon YouTube\n\`\`\`" > Log.txt
 
 echo "  $Vidon"
 if [ "$VERSION" == 'Auto' ];then
@@ -204,7 +204,7 @@ echo "▼ Bắt đầu quá trình xây dựng..."
 eval "java -Djava.io.tmpdir=$HOME -jar $lib1 patch -b $lib2 -m $lib3 apk/YouTube.apk -o YT.apk "$Tof $Ton $Mro $theme $feature" >> Log.txt 2>> Log.txt"
 sed '/WARNING: warn: removing resource/d' Log.txt
 echo '- Quá trình xây dựng apk xong.' | tee 2.txt
-echo "```" >> Log.txt
+echo "\`\`\`" >> Log.txt
 
 ) & (
 
