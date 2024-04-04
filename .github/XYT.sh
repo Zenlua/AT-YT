c868d485fb9395eea26eab18238ec6be8112b6d0
Kẹo Kéo <kakathic@gmail.com>
2024-04-04 12:32:29 +0700

    Update XYT.sh

--- .github/XYT.sh
+++ .github/XYT.sh
@@ -184,7 +184,7 @@ echo "▼ Bắt đầu quá trình xây dựng..."
 eval "java -Djava.io.tmpdir=$HOME -jar $lib1 patch -b $lib2 -m $lib3 apk/YouTube.apk -o YT.apk "$Tof $Ton $Mro $theme $feature"" >> Log2.txt 2>> Log2.txt
 #sed '/WARNING: warn: removing resource/d' Log.txt
 echo '- Quá trình xây dựng apk xong.' | tee 2.txt
-grep 'SEVERE:' Log2.txt > Log.txt
+grep 'SEVERE:' Log2.txt | sed 's|failed:|failed|g' > Log.txt
 
 ) & (
 
