bbdef4057ac61c24558e28249a4f68d072ceb522
Kẹo Kéo <kakathic@gmail.com>
2024-05-21 13:50:12 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -202,12 +202,13 @@ fi
 
 echo "▼ Bắt đầu quá trình xây dựng..."
 #java -Djava.io.tmpdir=$HOME -jar $lib1 patch
-eval "java -Djava.io.tmpdir=$HOME -jar $lib1 patch -b $lib2 -m $lib3 apk/YouTube.apk -t rmp -o YT.apk "$Tof $Ton $Mro $theme $feature""
+eval "java -Djava.io.tmpdir=$HOME -jar $lib1 patch -b $lib2 -m $lib3 apk/YouTube.apk -o YT.apk "$Tof $Ton $Mro $theme $feature""
 #cat Log2.txt
 #sed '/WARNING: warn: removing resource/d' Log2.txt
 #grep 'SEVERE:' Log2.txt | sed 's|failed:|failed|g' > Log.txt
 echo '- Quá trình xây dựng apk xong.' | tee 2.txt
-
+ls $HOME/
+ls $HOME/*
 ) & (
 
 sleep 5
