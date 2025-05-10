fcbd9e0755d105acab0d7a169ff92517d56c8e96
Kẹo Kéo <kakathic@gmail.com>
2025-05-10 14:21:03 +0700

    Update service.sh

--- .github/Modun/common/service.sh
+++ .github/Modun/common/service.sh
@@ -1,9 +1,10 @@
 # kakathic
-RD="$RANDOM"
+MODPATH="${0%/*}"
+rm -fr $MODPATH/YouTube/*
+
 while true; do
 [ -e /sdcard/Android ] && break || sleep 1
 done
-MODPATH="${0%/*}"
 . $MODPATH/YT.sh
 
 if [ "$(ls -l $(linkAPK) | awk '{print $5}')" == "$(cat $MODPATH/SIZE)" ];then
@@ -13,7 +14,7 @@ offCH
 else
 installYT $MODPATH/base.apk
 ls -l "$MODPATH/YouTube.apk" | awk '{print $5}' > $MODPATH/SIZE
-cpLIB $MODPATH/lib "$(linkAPK)"
+[ -e $MODPATH/lib ] && cpLIB $MODPATH/lib "$(linkAPK)"
 mountYT $MODPATH/YouTube.apk "$(linkAPK)"
 offCH
 fi
