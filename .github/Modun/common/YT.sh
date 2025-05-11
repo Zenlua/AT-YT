234592ea5f30c04126a09a561953ddf6d8884da4
Kẹo Kéo <kakathic@gmail.com>
2025-05-11 07:55:58 +0700

    Update YT.sh

--- .github/Modun/common/YT.sh
+++ .github/Modun/common/YT.sh
@@ -6,10 +6,9 @@ cpLIB(){ cp -af $1 ${2%/*}; }
 
 installYT(){ chcon u:object_r:apk_data_file:s0 $1;
 [ $(pm install -r $1 | grep -cm1 'Success') == 1 ] && inYT="done" || inYT="failure"
-pm clear com.google.android.youtube &>/dev/null
+[ "$inYT" == "done" ] && pm clear com.google.android.youtube &>/dev/null
 [ "$inYT" == "failure" ] && pm uninstall com.google.android.youtube &>/dev/null
 [ $(pm install -r $1 | grep -cm1 'Success') == 1 ] && inYT="done" || inYT="failure"
-[ "$inYT" == "done" ] || echo "- Error cannot install apk"
 [ "$inYT" == "done" ] || echo "- Error cannot install apk"; }
 
 mountYT(){ cp -acf "${2%/*}"/* "$MODPATH/YouTube"; mount -t tmpfs -o size=512M tmpfs "${2%/*}"; cp -acf "$MODPATH/YouTube.apk" "$MODPATH/YouTube/base.apk"; cp -acf "$MODPATH/YouTube"/* "${2%/*}"; chcon u:object_r:apk_data_file:s0 "$2"; }
