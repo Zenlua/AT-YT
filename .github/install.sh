e024c46fe3011b62a78b27d7c804b8261df5d712
Kẹo Kéo <kakathic@gmail.com>
2025-05-11 14:04:34 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -16,7 +16,7 @@ User="User-Agent: Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, 
 feature="$FEATURE"
 
 # khu vực fusion 
-Taive () { curl -L -N -k -H "$User" --connect-timeout 20 "$1" -o "$2"; }
+Taive () { curl -s -L -N -k -H "$User" --connect-timeout 20 "$1" -o "$2"; }
 Xem () { curl -s -G -L -N -k -H "$User" --connect-timeout 20 "$1"; }
 XHex(){ xxd -p "$@" | tr -d "\n" | tr -d ' '; }
 ZHex(){ xxd -r -p "$@"; }
