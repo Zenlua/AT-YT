961fe9aa5257bb009d42f7d542a3c0efd2336bad
Kẹo Kéo <kakathic@gmail.com>
2025-05-30 07:44:15 +0700

    Update XYT.sh

--- .github/XYT.sh
+++ .github/XYT.sh
@@ -220,13 +220,13 @@ echo '{
 }' > "Up-X$V$ach$amoled2.json"
 
 echo -e 'Update '$date' \nYouTube: '$VER' \nVersion: '${VER//./}'\nAuto by kakathic' > Up-X${V}notes.json
-Upenv BODY "**Note: Auto by kakathic**
+echo "**Note: Auto by kakathic**
 
 + Update $date
 + YouTube: $VER
 + Version: ${VER//./}
 
-+ ![GitHub Downloads (all assets, specific tag)](https://img.shields.io/github/downloads/$GITHUB_REPOSITORY/X$V$VER/total?label=Download&color=%230072F4)"
++ ![GitHub Downloads (all assets, specific tag)](https://img.shields.io/github/downloads/$GITHUB_REPOSITORY/X$V$VER/total?label=Download&color=%230072F4)" > change.txt
 
 # Tạo module magisk
 cd $HOME/.github/Modun
