e894b6090c16eb37977795b72dd66e650ba8486e
Kakathic <kakathic@gmail.com>
2025-08-07 10:39:44 +0700

    Update XYT.sh

--- .github/XYT.sh
+++ .github/XYT.sh
@@ -224,7 +224,6 @@ echo "**Note: Auto by kakathic**
 
 + Update $date
 + YouTube: $VER
-+ Version: ${VER//./}
 
 + ![GitHub Downloads (all assets, specific tag)](https://img.shields.io/github/downloads/$GITHUB_REPOSITORY/X$V$VER/total?label=Download&color=%230072F4)" > change.txt
 
