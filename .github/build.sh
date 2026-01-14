c623ca25e624a4e905b2202d3b1245031708941d
Kakathic <kakathic@gmail.com>
2026-01-14 21:50:49 +0700

    Update build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -159,6 +159,26 @@ echo
 ls YT-temporary-files/*.apk
 cp -rf YT-temporary-files/*.apk YT2.apk
 
+# Chờ xây dựng xong
+if [ "$TYPE" == 'true' ];then
+echo "Tạo rsign..."
+echo
+mv YT.apk $HOME/Tav/YouTube.apk
+cd tmp
+zip -qr $HOME/YT2.apk *
+cd $HOME
+rsign Tav/base.apk YT2.apk $HOME/Up/ZT-$VER-$ach${amoled2}-rsign.apk
+else
+apksign YT.apk $HOME/Up/YT-$VER-$ach${amoled2}.apk
+ls Up
+exit 0
+fi
+
+cd Tav
+tar -cf - * | xz -9kz > $HOME/.github/Modun/common/lib.tar.xz
+cd $HOME
+
+
 
 
 
