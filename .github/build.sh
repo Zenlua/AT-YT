cbb9d47c0d8645de41ab91d4be9763cbe70211ec
Kakathic <kakathic@gmail.com>
2026-01-15 06:41:22 +0700

    Update build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -208,7 +208,7 @@ echo "**Note: Auto by kakathic**
 + Update $date
 + YouTube: $VER
 
-+ ![GitHub Downloads (all assets, specific tag)](https://img.shields.io/github/downloads/$GITHUB_REPOSITORY/K$V$VER/total?label=Download&color=%230072F4)" > change.txt
++ ![GitHub Downloads (all assets, specific tag)](https://img.shields.io/github/downloads/$GITHUB_REPOSITORY/K-$V-$VER/total?label=Download&color=%230072F4)" > change.txt
 
 Upout change.txt
 
