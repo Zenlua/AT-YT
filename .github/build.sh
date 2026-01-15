b2856dc39a2f67f244005bbdbd32a00639a68660
Kakathic <kakathic@gmail.com>
2026-01-15 14:00:43 +0700

    Update build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -4,6 +4,9 @@
 HOME="$GITHUB_WORKSPACE"
 date="$(TZ=Asia/Ho_Chi_Minh date +"%Y-%m-%d %H:%M:%S.%3N GMT%Z")"
 
+echo "$date"
+echo
+
 # Tạo thư mục
 mkdir -p apk lib tmp jar Tav Up rmp
 User="User-Agent: Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Mobile Safari/537.36"
