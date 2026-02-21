06ec55d08f6955e59fe9cad0daf69f762f9ea2be
Kakathic <kakathic@gmail.com>
2026-02-21 12:15:05 +0700

    Update build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -11,7 +11,7 @@ echo
 # Tạo thư mục
 mkdir -p apk lib tmp jar Tav Up rmp
 User="User-Agent: Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Mobile Safari/537.36"
-
+set -x
 # Tính năng 
 feature="$FEATURE"
 
