55e3df16ce2f826a9ff85a8e71555432ed128a06
Kakathic <kakathic@gmail.com>
2026-02-26 20:44:43 +0700

    Update build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -264,7 +264,7 @@ chat_tg="New version of the patch tool
 
 • Mod by: ${V^}
 • Youtube version: $VER
-• <a href=\"https://github.com/Zenlua/AT-YT/releases/tag/K-$V-$VER-$Kad\">Download</a>
+• Link: <a href=\"https://github.com/Zenlua/AT-YT/releases/tag/K-$V-$VER-$Kad\">Download</a>
 • <a href=\"https://github.com/$GITPATCH/releases/latest\">Changelog</a>
 
 #youtube #bot_auto"
