5e49ac39ff5a6db0c5b5069674b22195b31eeb06
Kakathic <kakathic@gmail.com>
2026-03-08 22:48:08 +0700

    Update build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -134,7 +134,7 @@ if [ -e apk/YouTube$vv.zip ];then
     if [ "$(unzip -l apk/YouTube$vv.zip | grep -cm1 'base.apk')" == 1 ];then
     echo "- Apk $vv thành apks"
     mv apk/YouTube$vv.zip apk/YouTube.apks
-    elif [ "$(unzip -l apk/YouTube$vv.zip | grep -cm1 'AndroidManifest.xml'))" == 1 ];then
+    elif [ "$(unzip -l apk/YouTube$vv.zip | grep -cm1 'AndroidManifest.xml')" == 1 ];then
     echo "- Apk $vv thành apk"
     mv apk/YouTube$vv.zip apk/YouTube.apk
     else
