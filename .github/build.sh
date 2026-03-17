a76b3dd1ea5a2d56c60ba3f9c7027f3381376d01
Kakathic <kakathic@gmail.com>
2026-03-17 11:25:28 +0700

    Remove Java version listing from build.sh
    Removed command to list Java versions from build script.

--- .github/build.sh
+++ .github/build.sh
@@ -97,8 +97,6 @@ lib="lib/arm64-v8a/* lib/x86/* lib/x86_64/*"
 ach="arm"
 fi
 
-java -Djava.io.tmpdir=$HOME -jar cli.jar list-versions
-
 echo
 echo "- Kiểm tra bản YouTube mới nhất..."
 Vidon="$(java -Djava.io.tmpdir=$HOME -jar cli.jar list-versions -p patch.jar -f com.google.android.youtube | grep -w '(.*.)' | sort -n | tail -1 | awk '{print $1}')";
